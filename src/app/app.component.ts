1c053284402689e056275569256b3a756c7fc81a
jason-892019 <58570697+jason-892019@users.noreply.github.com>
2022-04-23 19:37:03 +0100

    Add files via upload
    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 
@@ -74,3 +75,81 @@ export class AppComponent implements OnInit {
     })
   }
 }
+=======
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-firebase';
+
+  dataSource : any;
+  id : any;
+  name : any;
+  personalInfo : any;
+  editObj : any;
+
+  @ViewChild('btnShow')
+  btnShow!: ElementRef;
+  @ViewChild('btnClose')
+  btnClose!: ElementRef;
+
+
+
+  constructor(private store: AngularFirestore){
+    
+  }
+
+  ngOnInit(){
+    this.getAll();
+  }
+
+  openDialog(){
+    this.btnShow.nativeElement.click();
+  }
+
+  closeDialog(){
+    this.btnClose.nativeElement.click();
+  }
+
+  clearEdit(){
+    this.editObj = null;
+    this.name = "";
+    this.personalInfo = "";
+  }
+
+  add(){
+    if(this.editObj){
+      this.store.collection('/user').doc(this.editObj.id).update({name : this.name, personalInfo : this.personalInfo});
+    } else {
+      this.store.collection('/user').add({name : this.name, personalInfo : this.personalInfo});
+    }
+    this.closeDialog();
+  }
+
+  edit(id : string){
+    this.store.collection('/user').doc(id).get().subscribe((response) => {
+      this.editObj = Object.assign({id : response.id}, response.data());
+      this.name = this.editObj.name;
+      this.personalInfo = this.editObj.personalInfo;
+      this.openDialog();
+    })
+  }
+
+  delete(id : string){
+    this.store.collection('/user').doc(id).delete();
+  }
+
+  getAll(){
+    this.store.collection('/user').snapshotChanges().subscribe((response) => {
+      this.dataSource = response.map(item => {
+        return Object.assign({id : item.payload.doc.id}, item.payload.doc.data())
+      });
+    })
+  }
+}
+>>>>>>> 5219419360f0de9fdf4783f1cb518e4d95ccfc2c
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -39,3 +40,46 @@ import { NavComponent } from './nav.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+=======
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
+import { HomeComponent } from './home.component';
+import { BrowseComponent } from './browse.component';
+import { CheckoutComponent } from './checkout.component';
+import { NgxPayPalModule } from 'ngx-paypal';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
+
+import { NavComponent } from './nav.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    HomeComponent,
+    BrowseComponent,
+    CheckoutComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    NgxPayPalModule,
+    GooglePayButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+>>>>>>> 5219419360f0de9fdf4783f1cb518e4d95ccfc2c
